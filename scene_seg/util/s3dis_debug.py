0b9dc528ed30c67acb758e1043cc5c76c0ac3d42
13952522076 <xuma@my.unt.edu>
2021-06-21 15:46:12 -0400

    update

--- scene_seg/util/s3dis_debug.py
+++ scene_seg/util/s3dis_debug.py
@@ -42,11 +42,12 @@ class S3DIS(Dataset):
     def __getitem__(self, idx):
         room_idx = self.room_idxs[idx]
         points = self.room_points[room_idx]   # N * 6
+        print(points.shape)
         labels = self.room_labels[room_idx]   # N
         N_points = points.shape[0]
         print(f"room_idx: {room_idx}| points shape:{ points.shape} | labels shape:{labels.shape}")
-        print(f"Range: x:[{min(points[:,0])}-{max(points[:,0])}] | y:[{min(points[:,1])}-{max(points[:,1])}] | "
-              f"z:[{min(points[:,2])}-{max(points[:,2])}]")
+        # print(f"Range: x:[{min(points[:,0])}-{max(points[:,0])}] | y:[{min(points[:,1])}-{max(points[:,1])}] | "
+        #       f"z:[{min(points[:,2])}-{max(points[:,2])}]")
         """
         while (True):
             # to select center points that at least 1024 points are covered in a block size 1m*1m
