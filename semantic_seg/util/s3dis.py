96f4fbd2e7407471d5a2f4548f567b40b71ea590
13952522076 <xuma@my.unt.edu>
2021-06-22 20:00:50 -0400

    update

--- semantic_seg/util/s3dis.py
+++ semantic_seg/util/s3dis.py
@@ -5,10 +5,11 @@ import torch
 from torch.utils.data import Dataset
 
 class S3DIS(Dataset):
-    def __init__(self, split='train', data_root='trainval_fullarea', num_point=4096, test_area=5, 
+    def __init__(self, split='train', data_root='trainval_fullarea', num_point=4096, test_area=5,
                  block_size=1.0, sample_rate=1.0, transform=None, fea_dim=6, shuffle_idx=False):
 
         super().__init__()
+        self.split = split
         self.num_point = num_point
         self.block_size = block_size
         self.transform = transform
@@ -45,28 +46,10 @@ class S3DIS(Dataset):
         labels = self.room_labels[room_idx]   # N
         N_points = points.shape[0]
         # print(f"room_idx: {room_idx}| points shape:{ points.shape} | labels shape:{labels.shape}")
-        while (True):
-            # to select center points that at least 1024 points are covered in a block size 1m*1m
-            center = points[np.random.choice(N_points)][:3]
-            block_min = center - [self.block_size / 2.0, self.block_size / 2.0, 0]
-            block_max = center + [self.block_size / 2.0, self.block_size / 2.0, 0]
-            point_idxs = np.where((points[:, 0] >= block_min[0]) & (points[:, 0] <= block_max[0]) & (points[:, 1] >= block_min[1]) & (points[:, 1] <= block_max[1]))[0]
-            if point_idxs.size > self.num_point / 4:
-                break
-
-        if point_idxs.size >= self.num_point:
-            selected_point_idxs = np.random.choice(point_idxs, self.num_point, replace=False)
-        else:
-            # do not use random choice here to avoid some pts not counted
-            dup = np.random.choice(point_idxs.size, self.num_point - point_idxs.size)
-            idx_dup = np.concatenate([np.arange(point_idxs.size), np.array(dup)], 0)
-            selected_point_idxs = point_idxs[idx_dup]
-
+        return N_points
+        """
         selected_points = points[selected_point_idxs, :]  # num_point * 6
-        # centered points
-        centered_points = np.zeros((self.num_point, 3))
-        centered_points[:, :2] = selected_points[:, :2] - center[:2]
-        centered_points[:, 2] = selected_points[:, 2]
+
         # normalized colors
         normalized_colors = selected_points[:, 3:6] / 255.0
         # normalized points
@@ -94,6 +77,7 @@ class S3DIS(Dataset):
         current_labels = torch.LongTensor(current_labels)
  
         return current_points, current_labels
+        """
 
     def __len__(self):
         return len(self.room_idxs)
@@ -112,6 +96,21 @@ if __name__ == '__main__':
                                          transform.RandomDropColor(p=0.8, color_augment=0.0)])
     point_data = S3DIS(split='train', data_root=data_root, num_point=num_point, test_area=test_area, block_size=block_size, sample_rate=sample_rate, transform=train_transform)
     print('point data size:', point_data.__len__())
+
+    train_loader = torch.utils.data.DataLoader(point_data, batch_size=1, shuffle=True, num_workers=1,
+                                               pin_memory=True)
+    max_points = 0
+    min_points = 9999999999
+    from tqdm import tqdm
+    for i, (n_points) in tqdm(enumerate(train_loader)):
+        if n_points > max_points:
+            max_points = n_points
+        if n_points < min_points:
+            min_points = n_points
+
+    print(f"max_points: {max_points} | min_points: {min_points}")
+    quit(0)
+
 
     print('point data 0 shape:', point_data.__getitem__(0)[0].shape)
     print('point label 10000 shape:', point_data.__getitem__(1000)[1].shape)
