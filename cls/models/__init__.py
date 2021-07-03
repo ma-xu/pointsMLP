ab31c38bbac0ed08df60fbefff5699e83588dadc
13952522076 <xuma@my.unt.edu>
2021-07-02 23:08:18 -0400

    update

--- cls/models/__init__.py
+++ cls/models/__init__.py
@@ -2,3 +2,5 @@ from __future__ import absolute_import
 from .pointnet import PointNet
 from .model21 import model21H
 from .model1 import model1A, model1B, model1C, model1D, model1E, model1F
+from .model2 import model2A1,model2A2, model2A3, model2A4, model2A5, model2A6, \
+    model2A7, model2A8, model2A9, model2A10, model2A11, model2A12
