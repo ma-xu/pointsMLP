85bcb395cec462ba9e7c9cbdf0a664ed21f561ca
13952522076 <xuma@my.unt.edu>
2021-07-04 16:31:14 -0400

    update

--- cls/ablation/compare_sampling.py
+++ cls/ablation/compare_sampling.py
@@ -38,3 +38,31 @@ def measure_time(args):
 if __name__ == '__main__':
     args = parse_args()
     measure_time(args)
+
+
+# salloc -N 1 -p multigpu --gres=gpu:v100-sxm2:1 --cpus-per-task=1 --mem=128Gb  --time=1-00:00:00
+# results
+# (point) [ma.xu1@d1009 ablation]$ python compare_sampling.py --num_points 1024 --sam_points 512 --iterations 500 --device cuda
+# Finishing warm up devices
+# 	[points:1024, sampling:512, iterations:500, device:cuda]  FPS time: 0.17862 | RAND time: 0.035436
+# (point) [ma.xu1@d1009 ablation]$
+# (point) [ma.xu1@d1009 ablation]$ python compare_sampling.py --num_points 2048 --sam_points 1024 --iterations 500 --device cuda
+# Finishing warm up devices
+# 	[points:2048, sampling:1024, iterations:500, device:cuda]  FPS time: 0.360224 | RAND time: 0.043737
+# (point) [ma.xu1@d1009 ablation]$ python compare_sampling.py --num_points 4096 --sam_points 2048 --iterations 500 --device cuda
+# Finishing warm up devices
+# 	[points:4096, sampling:2048, iterations:500, device:cuda]  FPS time: 0.915579 | RAND time: 0.061251
+# (point) [ma.xu1@d1009 ablation]$ python compare_sampling.py --num_points 8192 --sam_points 4096 --iterations 500 --device cuda
+# Finishing warm up devices
+# 	[points:8192, sampling:4096, iterations:500, device:cuda]  FPS time: 4.47864 | RAND time: 0.106079
+# (point) [ma.xu1@d1009 ablation]$ python compare_sampling.py --num_points 16384 --sam_points 8192 --iterations 500 --device cuda
+# Finishing warm up devices
+# 	[points:16384, sampling:8192, iterations:500, device:cuda]  FPS time: 16.085766 | RAND time: 0.211724
+# (point) [ma.xu1@d1009 ablation]$ python compare_sampling.py --num_points 32768 --sam_points 16384 --iterations 500 --device cuda
+# Finishing warm up devices
+# 	[points:32768, sampling:16384, iterations:500, device:cuda]  FPS time: 59.364385 | RAND time: 0.510331
+# (point) [ma.xu1@d1009 ablation]$ python compare_sampling.py --num_points 65536 --sam_points 32768 --iterations 500 --device cuda
+# Finishing warm up devices
+# 	[points:65536, sampling:32768, iterations:500, device:cuda]  FPS time: 227.105271 | RAND time: 1.44456
+# (point) [ma.xu1@d1009 ablation]$
+#
