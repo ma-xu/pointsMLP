ae49fe2b6f4cea47ff58675096e3abeec61385c1
13952522076 <xuma@my.unt.edu>
2021-07-16 18:34:57 -0400

    update

--- partseg_gdaversion/model/__init__.py
+++ partseg_gdaversion/model/__init__.py
@@ -3,3 +3,6 @@ from .GDANet_ptseg import GDANet
 from .pointMLP1 import PointMLP1
 from .pointMLP2 import PointMLP2
 from .pointMLP3 import PointMLP3
+from .pointMLP4 import PointMLP4
+from .pointMLP5 import PointMLP5
+from .pointMLP6 import PointMLP6
