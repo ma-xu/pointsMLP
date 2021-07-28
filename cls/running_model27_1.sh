d59ccd97ede0a412f50cb2383bd6cc2b0fcdc441
13952522076 <xuma@my.unt.edu>
2021-07-27 20:15:16 -0400

    update

--- cls/running_model27_1.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-python main.py --model model27A
-python main.py --model model27B
-python main.py --model model27C
-python main.py --model model27D
-
---
+++ cls/running_model30_2.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+python main.py --model model30J --msg demo2
+python main.py --model model30I --msg demo2
+python main.py --model model30H --msg demo2
+python main.py --model model30G --msg demo2
+python main.py --model model30F --msg demo2
+python main.py --model model30E --msg demo2
+python main.py --model model30D --msg demo2
+python main.py --model model30C --msg demo2
+python main.py --model model30B --msg demo2
+python main.py --model model30A --msg demo2
