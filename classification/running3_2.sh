90c1604957441a8be590cd3ce5ae23f1062be028
13952522076 <xuma@my.unt.edu>
2021-08-08 02:34:28 -0400

    update

--- classification/running3_2.sh
+++ classification/running3_2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-python main.py --model model31K --msg epoch350_2
+
 python main.py --model model31L --msg epoch350_2
 python main.py --model model31M --msg epoch350_2
 python main.py --model model31N --msg epoch350_2
