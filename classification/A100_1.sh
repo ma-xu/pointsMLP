6f3928bb4aa06a6f640cbd7db5fce82a24833672
13952522076 <xuma@my.unt.edu>
2021-08-08 02:56:00 -0400

    update

---
+++ classification/A100_1.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+python main.py --model model32A  --epoch 300 --msg epoch300_1
+python main.py --model model32C  --epoch 300 --msg epoch300_1
+python main.py --model model32D  --epoch 300 --msg epoch300_1
+python main.py --model model32E  --epoch 300 --msg epoch300_1
+python main.py --model model32F  --epoch 300 --msg epoch300_1
+python main.py --model model32G  --epoch 300 --msg epoch300_1
---
+++ classification/A100_7.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+python main.py --model model32H  --epoch 300 --msg epoch300_3
+python main.py --model model32I  --epoch 300 --msg epoch300_3
+python main.py --model model32J  --epoch 300 --msg epoch300_3
+python main.py --model model32K  --epoch 300 --msg epoch300_3
+python main.py --model model32L  --epoch 300 --msg epoch300_3
+python main.py --model model32M  --epoch 300 --msg epoch300_3
+python main.py --model model32N  --epoch 300 --msg epoch300_3
