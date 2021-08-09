4e02a725cd6e67182972d1fd81e0270547c2f5e4
13952522076 <xuma@my.unt.edu>
2021-08-09 11:31:20 -0400

    update

--- classification/A100_4.sh
+++ classification/A100_4.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-python main.py --model model32C  --epoch 300 --msg epoch300_4
 python main.py --model model32D  --epoch 300 --msg epoch300_4
 python main.py --model model32E  --epoch 300 --msg epoch300_4
 python main.py --model model32F  --epoch 300 --msg epoch300_4
