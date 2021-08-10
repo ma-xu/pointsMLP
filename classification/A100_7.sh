1b585640c4ad4ab095e93b4ca3f295a1d1e8dfc5
13952522076 <xuma@my.unt.edu>
2021-08-10 02:46:02 -0400

    update

--- classification/A100_7.sh
+++ classification/A100_7.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-python main.py --model model32K  --epoch 300 --msg epoch300_3
-python main.py --model model32L  --epoch 300 --msg epoch300_3
-python main.py --model model32M  --epoch 300 --msg epoch300_3
-python main.py --model model32N  --epoch 300 --msg epoch300_3
+#python main.py --model model32K  --epoch 300 --msg epoch300_3
+#python main.py --model model32L  --epoch 300 --msg epoch300_3
+#python main.py --model model32M  --epoch 300 --msg epoch300_3
+#python main.py --model model32N  --epoch 300 --msg epoch300_3
+
+
+python main.py --model model31A --seed 42 --epoch 300 --msg epoch300_seed42
+python main.py --model model31A --seed 1234 --epoch 300 --msg epoch300_seed1234
+python main.py --model model31A --batch_size 16 --epoch 300 --msg epoch300_bs16_7
--- classification/A100_8.sh
+++ classification/A100_8.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-python main.py --model model32K  --epoch 300 --msg epoch300_4
-python main.py --model model32L  --epoch 300 --msg epoch300_4
-python main.py --model model32M  --epoch 300 --msg epoch300_4
-python main.py --model model32N  --epoch 300 --msg epoch300_4
+#python main.py --model model32K  --epoch 300 --msg epoch300_4
+#python main.py --model model32L  --epoch 300 --msg epoch300_4
+#python main.py --model model32M  --epoch 300 --msg epoch300_4
+#python main.py --model model32N  --epoch 300 --msg epoch300_4
+
+
+python main.py --model model31A --seed 111 --epoch 300 --msg epoch300_seed111
+python main.py --model model31A --seed 111 --epoch 300 --msg epoch300_seed111
+python main.py --model model31A --batch_size 16 --epoch 300 --msg epoch300_bs16_8
