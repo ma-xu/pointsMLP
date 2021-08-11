a9d205f131fadb94d10aae3348442262474996bb
13952522076 <xuma@my.unt.edu>
2021-08-11 01:58:44 -0400

    update

---
+++ classification/list_results.py
@@ -0,0 +1,46 @@
+"""
+for training with resume functions.
+Usage:
+python main.py --model PointNet --msg demo
+or
+CUDA_VISIBLE_DEVICES=0 nohup python main.py --model PointNet --msg demo > nohup/PointNet_demo.out &
+"""
+import argparse
+import os
+import logging
+import datetime
+import torch
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torch.utils.data.distributed
+from torch.utils.data import DataLoader
+
+
+def parse_args():
+    """Parameters"""
+    parser = argparse.ArgumentParser('training')
+    parser.add_argument('--path', default='./checkpoints', type=str)
+    return parser.parse_args()
+
+
+def main():
+        args = parse_args()
+        models = os.listdir(args.path)
+        for model in models:
+            if os.path.isfile(os.path.join(args.path, model, "best_checkpoint.pth")):
+                checkpoint_path = os.path.join(args.checkpoint, "last_checkpoint.pth")
+                checkpoint = torch.load(checkpoint_path)
+                epoch = checkpoint['epoch']
+                best_test_acc = checkpoint['best_test_acc']
+
+                print(f"{model}: epoch{epoch}  best_test_acc: {best_test_acc}")
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
