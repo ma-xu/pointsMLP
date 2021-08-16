2fc076e05c9eb0b2ce33c0035aa3da6d22153c94
Xu Ma <xuma@my.unt.edu>
2021-08-16 01:45:29 -0400

    update

--- classification/running3_4.sh
+++ classification/running3_4.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 
-python main.py --model model31L --msg epoch350_4
-python main.py --model model31M --msg epoch350_4
-python main.py --model model31N --msg epoch350_4
+python main.py --model model32H --msg update_4
+python main.py --model model32I --msg update_4
