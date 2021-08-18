b79bc5b562a7878ab4e4ca27a41aec661ffe90d5
Xu Ma <xuma@my.unt.edu>
2021-08-18 02:26:07 -0400

    Update running_1.sh

--- classification/running_1.sh
+++ classification/running_1.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-python main.py --model model33A --msg update1
-python main.py --model model33B --msg update1
-python main.py --model model33C --msg update1
-python main.py --model model33D --msg update1
+#python main.py --model model33A --msg update1
+#python main.py --model model33B --msg update1
+#python main.py --model model33C --msg update1
+#python main.py --model model33D --msg update1
+python main.py --model model31A
+python main.py --model model31A
+python main.py --model model31A
+python main.py --model model31A
+python main.py --model model31A
+python main.py --model model31A
+python main.py --model model31A
+python main.py --model model31A
