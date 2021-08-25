83f5ee0d92697c7c301e860fd6f6608461373218
Xu Ma <xuma@my.unt.edu>
2021-08-25 15:41:08 -0400

    Update read_file.sh

--- read_file.sh
+++ read_file.sh
@@ -2,8 +2,8 @@
 
 # usage:
 #  bash read_file.sh /scratch/ma.xu1/pointsMLP/classification/checkpoints/model31A-20210818204651
-folder = $1
+dir = $1
 
-for file in $folder; do
+for file in $dir/*; do
     echo $file
 done
