e1d979683043d81cdc2b625f46be7e12574d9a0a
Xu Ma <xuma@my.unt.edu>
2021-08-25 15:38:44 -0400

    Create read_file.sh

---
+++ read_file.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# usage:
+#  bash read_file.sh /scratch/ma.xu1/pointsMLP/classification/checkpoints/model31A-20210818204651
+folder = $1
+
+for file in folder/*; do
+    echo $file
+done
