768e79f1c3fbb84e1b6ad6c18a254cc992e6d5ba
Xu Ma <xuma@my.unt.edu>
2021-08-26 02:26:36 -0400

    update

--- partseg_curvenet/models/__init__.py
+++ partseg_curvenet/models/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import absolute_import
 from .curvenet_seg import CurveNet
-from .pointMLP31 import model31G, model31J, model31K
+from .pointMLP31 import model31G, model31J, model31K, model31D, model31A
