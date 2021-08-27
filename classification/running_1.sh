d07e282d6aea499a9d7fc8a4d01bb5f2c8dcacdd
Xu Ma <xuma@my.unt.edu>
2021-08-26 23:09:22 -0400

    update

--- classification/running_1.sh
+++ classification/running_1.sh
@@ -4,11 +4,11 @@
 #python main.py --model model33B --msg update1
 #python main.py --model model33C --msg update1
 #python main.py --model model33D --msg update1
-python main.py --model model31A --epoch 300
-python main.py --model model31A --epoch 300
-python main.py --model model31A --epoch 300
-python main.py --model model31A --epoch 300
-python main.py --model model31A --epoch 300
-python main.py --model model31A --epoch 300
-python main.py --model model31A --epoch 300
-python main.py --model model31A --epoch 300
+python main.py --model model31A
+python main.py --model model31A
+python main.py --model model31A
+python main.py --model model31A
+python main.py --model model31A
+python main.py --model model31A
+python main.py --model model31A
+python main.py --model model31A
---
+++ classification/running_epoch300.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+python main.py --model model31A --epoch 300
+python main.py --model model31A --epoch 300
+python main.py --model model31A --epoch 300
+python main.py --model model31A --epoch 300
+python main.py --model model31A --epoch 300
+python main.py --model model31A --epoch 300
+python main.py --model model31A --epoch 300
+python main.py --model model31A --epoch 300
