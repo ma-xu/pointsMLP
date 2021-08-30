887bdfeaebe3b962194872d6c8c6b48d07889153
Xu Ma <xuma@my.unt.edu>
2021-08-30 16:03:42 -0400

    update

--- partseg_gdaversion/main_ptseg.py
+++ partseg_gdaversion/main_ptseg.py
@@ -19,6 +19,43 @@ import random
 
 classes_str = ['aero','bag','cap','car','chair','ear','guitar','knife','lamp','lapt','moto','mug','Pistol','rock','stake','table']
 
+def parse_args():
+    # Training settings
+    parser = argparse.ArgumentParser(description='3D Shape Part Segmentation')
+    parser.add_argument('--model', type=str, default='PointMLP1')
+    parser.add_argument('--exp_name', type=str, default='demo1', metavar='N',
+                        help='Name of the experiment')
+    parser.add_argument('--batch_size', type=int, default=32, metavar='batch_size',
+                        help='Size of batch)')
+    parser.add_argument('--test_batch_size', type=int, default=32, metavar='batch_size',
+                        help='Size of batch)')
+    parser.add_argument('--epochs', type=int, default=350, metavar='N',
+                        help='number of episode to train')
+    parser.add_argument('--use_sgd', type=bool, default=False,
+                        help='Use SGD')
+    parser.add_argument('--scheduler', type=str, default='step',
+                        help='lr scheduler')
+    parser.add_argument('--step', type=int, default=40,
+                        help='lr decay step')
+    parser.add_argument('--lr', type=float, default=0.003, metavar='LR',
+                        help='learning rate')
+    parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
+                        help='SGD momentum (default: 0.9)')
+    parser.add_argument('--no_cuda', type=bool, default=False,
+                        help='enables CUDA training')
+    parser.add_argument('--manual_seed', type=int, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--eval', type=bool,  default=False,
+                        help='evaluate the model')
+    parser.add_argument('--num_points', type=int, default=2048,
+                        help='num of points to use')
+    parser.add_argument('--resume', type=bool, default=False,
+                        help='Resume training or not')
+    parser.add_argument('--model_type', type=str, default='insiou',
+                        help='choose to test the best insiou/clsiou/acc model (options: insiou, clsiou, acc)')
+    return parser.parse_args()
+
+
 
 def _init_():
     if not os.path.exists('checkpoints'):
@@ -27,29 +64,7 @@ def _init_():
         os.makedirs('checkpoints/' + args.exp_name)
 
 
-def weight_init(m):
-    if isinstance(m, torch.nn.Linear):
-        torch.nn.init.xavier_normal_(m.weight)
-        if m.bias is not None:
-            torch.nn.init.constant_(m.bias, 0)
-    elif isinstance(m, torch.nn.Conv2d):
-        torch.nn.init.xavier_normal_(m.weight)
-        if m.bias is not None:
-            torch.nn.init.constant_(m.bias, 0)
-    elif isinstance(m, torch.nn.Conv1d):
-        torch.nn.init.xavier_normal_(m.weight)
-        if m.bias is not None:
-            torch.nn.init.constant_(m.bias, 0)
-    elif isinstance(m, torch.nn.BatchNorm2d):
-        torch.nn.init.constant_(m.weight, 1)
-        torch.nn.init.constant_(m.bias, 0)
-    elif isinstance(m, torch.nn.BatchNorm1d):
-        torch.nn.init.constant_(m.weight, 1)
-        torch.nn.init.constant_(m.bias, 0)
-
-
 def train(args, io):
-
     # ============= Model ===================
     num_part = 50
     device = torch.device("cuda" if args.cuda else "cpu")
@@ -57,14 +72,24 @@ def train(args, io):
     model = models.__dict__[args.model](num_part).to(device)
     io.cprint(str(model))
 
-    model.apply(weight_init)
     model = nn.DataParallel(model)
-    print("Let's use", torch.cuda.device_count(), "GPUs!")
+    print("Let's use ", torch.cuda.device_count(), " GPUs!")
+
+    best_acc = 0
+    best_class_iou = 0
+    best_instance_iou = 0
+    optimizer_dict = None
 
     '''Resume or not'''
+    start_epoch = 0 # start from epoch 0 or last checkpoint epoch
     if args.resume:
-        state_dict = torch.load("checkpoints/%s/best_insiou_model.pth" % args.exp_name,
-                                map_location=torch.device('cpu'))['model']
+        checkpoint = torch.load("checkpoints/%s/last_model.pth" % args.exp_name, map_location=torch.device('cpu'))
+        state_dict = checkpoint['model']
+        start_epoch = checkpoint['epoch']
+        best_acc = checkpoint['best_acc']
+        best_class_iou = checkpoint['best_class_iou']
+        best_instance_iou = checkpoint['best_instance_iou']
+        optimizer_dict = checkpoint['optimizer']
         for k in state_dict.keys():
             if 'module' not in k:
                 from collections import OrderedDict
@@ -75,47 +100,40 @@ def train(args, io):
             break
         model.load_state_dict(state_dict)
 
-        print("Resume training model...")
-        print(torch.load("checkpoints/%s/best_insiou_model.pth" % args.exp_name).keys())
+        print(f"Resume training model from epoch {start_epoch}...")
+        print(torch.load("checkpoints/%s/last_model.pth" % args.exp_name).keys())
     else:
         print("Training from scratch...")
 
     # =========== Dataloader =================
     train_data = PartNormalDataset(npoints=2048, split='trainval', normalize=False)
     print("The number of training data is:%d", len(train_data))
-
     test_data = PartNormalDataset(npoints=2048, split='test', normalize=False)
     print("The number of test data is:%d", len(test_data))
-
-    train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True, num_workers=6,
-                              drop_last=True)
-
-    test_loader = DataLoader(test_data, batch_size=args.test_batch_size, shuffle=False, num_workers=6,
-                             drop_last=False)
-
+    train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True, num_workers=8, drop_last=True)
+    test_loader = DataLoader(test_data, batch_size=args.test_batch_size, shuffle=False, num_workers=8, drop_last=False)
     # ============= Optimizer ================
     if args.use_sgd:
         print("Use SGD")
-        opt = optim.SGD(model.parameters(), lr=args.lr*100, momentum=args.momentum, weight_decay=0)
+        opt = optim.SGD(model.parameters(), lr=args.lr*100, momentum=args.momentum, weight_decay=1e-4)
     else:
         print("Use Adam")
-        opt = optim.Adam(model.parameters(), lr=args.lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)
-
+        opt = optim.Adam(model.parameters(), lr=args.lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=1e-4)
+    if optimizer_dict is not None:
+        opt.load_state_dict(optimizer_dict)
     if args.scheduler == 'cos':
         print("Use CosLR")
-        scheduler = CosineAnnealingLR(opt, args.epochs, eta_min=args.lr if args.use_sgd else args.lr / 100)
+        scheduler = CosineAnnealingLR(opt, args.epochs, eta_min=args.lr if args.use_sgd else args.lr /100,
+                                      last_epoch=start_epoch - 1)
     else:
         print("Use StepLR")
-        scheduler = StepLR(opt, step_size=args.step, gamma=0.5)
+        scheduler = StepLR(opt, step_size=args.step, gamma=0.5, last_epoch=start_epoch - 1)
 
     # ============= Training =================
-    best_acc = 0
-    best_class_iou = 0
-    best_instance_iou = 0
     num_part = 50
     num_classes = 16
 
-    for epoch in range(args.epochs):
+    for epoch in range(start_epoch, args.epochs):
 
         train_epoch(train_loader, model, opt, scheduler, epoch, num_part, num_classes, io)
 
@@ -124,7 +142,7 @@ def train(args, io):
         # 1. when get the best accuracy, save the model:
         if test_metrics['accuracy'] > best_acc:
             best_acc = test_metrics['accuracy']
-            io.cprint('Max Acc:%.5f' % best_acc)
+            io.cprint('saving new max Acc:%.5f' % best_acc)
             state = {
                 'model': model.module.state_dict() if torch.cuda.device_count() > 1 else model.state_dict(),
                 'optimizer': opt.state_dict(), 'epoch': epoch, 'test_acc': best_acc}
@@ -133,7 +151,7 @@ def train(args, io):
         # 2. when get the best instance_iou, save the model:
         if test_metrics['shape_avg_iou'] > best_instance_iou:
             best_instance_iou = test_metrics['shape_avg_iou']
-            io.cprint('Max instance iou:%.5f' % best_instance_iou)
+            io.cprint('saving new max instance iou:%.5f' % best_instance_iou)
             state = {
                 'model': model.module.state_dict() if torch.cuda.device_count() > 1 else model.state_dict(),
                 'optimizer': opt.state_dict(), 'epoch': epoch, 'test_instance_iou': best_instance_iou}
@@ -148,23 +166,30 @@ def train(args, io):
         if avg_class_iou > best_class_iou:
             best_class_iou = avg_class_iou
             # print the iou of each class:
+            io.cprint('saving new max class iou:%.5f' % best_class_iou)
             for cat_idx in range(16):
                 io.cprint(classes_str[cat_idx] + ' iou: ' + str(total_per_cat_iou[cat_idx]))
-            io.cprint('Max class iou:%.5f' % best_class_iou)
             state = {
                 'model': model.module.state_dict() if torch.cuda.device_count() > 1 else model.state_dict(),
                 'optimizer': opt.state_dict(), 'epoch': epoch, 'test_class_iou': best_class_iou}
             torch.save(state, 'checkpoints/%s/best_clsiou_model.pth' % args.exp_name)
 
+        # save last model
+        state = {
+            'model': model.module.state_dict() if torch.cuda.device_count() > 1 else model.state_dict(),
+            'optimizer': opt.state_dict(),
+            'epoch': epoch,
+            'best_acc':best_acc,
+            'best_class_iou':best_class_iou,
+            'best_instance_iou': best_instance_iou
+        }
+        torch.save(state, 'checkpoints/%s/last_model.pth' % (args.exp_name))
+
     # report best acc, ins_iou, cls_iou
     io.cprint('Final Max Acc:%.5f' % best_acc)
     io.cprint('Final Max instance iou:%.5f' % best_instance_iou)
     io.cprint('Final Max class iou:%.5f' % best_class_iou)
-    # save last model
-    state = {
-        'model': model.module.state_dict() if torch.cuda.device_count() > 1 else model.state_dict(),
-        'optimizer': opt.state_dict(), 'epoch': args.epochs - 1, 'test_iou': best_instance_iou}
-    torch.save(state, 'checkpoints/%s/model_ep%d.pth' % (args.exp_name, args.epochs))
+
 
 
 def train_epoch(train_loader, model, opt, scheduler, epoch, num_part, num_classes, io):
@@ -177,8 +202,8 @@ def train_epoch(train_loader, model, opt, scheduler, epoch, num_part, num_classe
 
     for batch_id, (points, label, target, norm_plt) in tqdm(enumerate(train_loader), total=len(train_loader), smoothing=0.9):
         batch_size, num_point, _ = points.size()
-        points, label, target, norm_plt = Variable(points.float()), Variable(label.long()), Variable(target.long()), \
-                                          Variable(norm_plt.float())
+        points, label, target, norm_plt = torch.Tensor(points.float()), torch.Tensor(label.long()), \
+                                          torch.Tensor(target.long()),torch.Tensor(norm_plt.float())
         points = points.transpose(2, 1)
         norm_plt = norm_plt.transpose(2, 1)
         points, label, target, norm_plt = points.cuda(non_blocking=True), label.squeeze(1).cuda(non_blocking=True), \
@@ -209,24 +234,17 @@ def train_epoch(train_loader, model, opt, scheduler, epoch, num_part, num_classe
         count += batch_size   # count the total number of samples in each iteration
         train_loss += loss.item() * batch_size
         accuracy.append(correct.item()/(batch_size * num_point))   # append the accuracy of each iteration
-
         # Note: We do not need to calculate per_class iou during training
 
-    if args.scheduler == 'cos':
-        scheduler.step()
-    elif args.scheduler == 'step':
-        if opt.param_groups[0]['lr'] > 0.9e-5:
-            scheduler.step()
-        if opt.param_groups[0]['lr'] < 0.9e-5:
-            for param_group in opt.param_groups:
-                param_group['lr'] = 0.9e-5
+
+    scheduler.step()
     io.cprint('Learning rate: %f' % opt.param_groups[0]['lr'])
 
     metrics['accuracy'] = np.mean(accuracy)
     metrics['shape_avg_iou'] = shape_ious * 1.0 / count
 
-    outstr = 'Train %d, loss: %f, train acc: %f, train ins_iou: %f' % (epoch+1, train_loss * 1.0 / count,
-                                                                       metrics['accuracy'], metrics['shape_avg_iou'])
+    outstr = 'Train epoch %d, lr: %.6f loss: %.5f, train acc: %.3f, train ins_iou: %.3f' % \
+             (epoch+1, opt.param_groups[0]['lr'], train_loss / count, metrics['accuracy'], metrics['shape_avg_iou'])
     io.cprint(outstr)
 
 
@@ -243,8 +261,8 @@ def test_epoch(test_loader, model, epoch, num_part, num_classes, io):
     # label_size: b, means each sample has one corresponding class
     for batch_id, (points, label, target, norm_plt) in tqdm(enumerate(test_loader), total=len(test_loader), smoothing=0.9):
         batch_size, num_point, _ = points.size()
-        points, label, target, norm_plt = Variable(points.float()), Variable(label.long()), Variable(target.long()), \
-                                          Variable(norm_plt.float())
+        points, label, target, norm_plt = torch.Tensor(points.float()), torch.Tensor(label.long()), \
+                                          torch.Tensor(target.long()), torch.Tensor(norm_plt.float())
         points = points.transpose(2, 1)
         norm_plt = norm_plt.transpose(2, 1)
         points, label, target, norm_plt = points.cuda(non_blocking=True), label.squeeze(1).cuda(non_blocking=True), \
@@ -286,7 +304,7 @@ def test_epoch(test_loader, model, epoch, num_part, num_classes, io):
     metrics['accuracy'] = np.mean(accuracy)
     metrics['shape_avg_iou'] = shape_ious * 1.0 / count
 
-    outstr = 'Test %d, loss: %f, test acc: %f  test ins_iou: %f' % (epoch + 1, test_loss * 1.0 / count,
+    outstr = 'Test epoch %d, loss: %f, test acc: %f  test ins_iou: %f \n' % (epoch + 1, test_loss / count,
                                                                     metrics['accuracy'], metrics['shape_avg_iou'])
 
     io.cprint(outstr)
@@ -374,48 +392,11 @@ def test(args, io):
 
 if __name__ == "__main__":
     # Training settings
-    parser = argparse.ArgumentParser(description='3D Shape Part Segmentation')
-    parser.add_argument('--model', type=str, default='PointMLP1')
-    parser.add_argument('--exp_name', type=str, default='demo1', metavar='N',
-                        help='Name of the experiment')
-    parser.add_argument('--batch_size', type=int, default=32, metavar='batch_size',
-                        help='Size of batch)')
-    parser.add_argument('--test_batch_size', type=int, default=32, metavar='batch_size',
-                        help='Size of batch)')
-    parser.add_argument('--epochs', type=int, default=350, metavar='N',
-                        help='number of episode to train')
-    parser.add_argument('--use_sgd', type=bool, default=False,
-                        help='Use SGD')
-    parser.add_argument('--scheduler', type=str, default='step',
-                        help='lr scheduler')
-    parser.add_argument('--step', type=int, default=40,
-                        help='lr decay step')
-    parser.add_argument('--lr', type=float, default=0.003, metavar='LR',
-                        help='learning rate')
-    parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
-                        help='SGD momentum (default: 0.9)')
-    parser.add_argument('--no_cuda', type=bool, default=False,
-                        help='enables CUDA training')
-    parser.add_argument('--manual_seed', type=int, metavar='S',
-                        help='random seed (default: 1)')
-    parser.add_argument('--eval', type=bool,  default=False,
-                        help='evaluate the model')
-    parser.add_argument('--num_points', type=int, default=1024,
-                        help='num of points to use')
-    parser.add_argument('--resume', type=bool, default=False,
-                        help='Resume training or not')
-    parser.add_argument('--model_type', type=str, default='insiou',
-                        help='choose to test the best insiou/clsiou/acc model (options: insiou, clsiou, acc)')
-
-    args = parser.parse_args()
+    args = parse_args()
     args.exp_name = args.model+"_"+args.exp_name
-
     _init_()
-
-    if not args.eval:
-        io = IOStream('checkpoints/' + args.exp_name + '/%s_train.log' % (args.exp_name))
-    else:
-        io = IOStream('checkpoints/' + args.exp_name + '/%s_test.log' % (args.exp_name))
+    temp = "train" if not args.eval else "test"
+    io = IOStream(f'checkpoints/{args.exp_name}/{temp}.log')
     io.cprint(str(args))
 
     if args.manual_seed is not None:
@@ -424,7 +405,6 @@ if __name__ == "__main__":
         torch.manual_seed(args.manual_seed)
 
     args.cuda = not args.no_cuda and torch.cuda.is_available()
-
     if args.cuda:
         io.cprint('Using GPU')
         if args.manual_seed is not None:
