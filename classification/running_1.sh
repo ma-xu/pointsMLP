52e44f79020a046c35f065b06c9e1591e11520ac
Xu Ma <xuma@my.unt.edu>
2021-08-30 03:04:45 -0400

    Update running_1.sh

--- classification/running_1.sh
+++ classification/running_1.sh
@@ -4,11 +4,11 @@
 #python main.py --model model33B --msg update1
 #python main.py --model model33C --msg update1
 #python main.py --model model33D --msg update1
-python main.py --model model31C --msg 4gpu34rtwnhr34245t
-python main.py --model model31C --msg 4gpu54tbtrf3g34g3d
-python main.py --model model31C --msg 4gpu7lk87kjyh4g3rg
-python main.py --model model31C --msg 4gpu4gvr3ece2f27sd
-python main.py --model model31C --msg 4gpu12dwdvrnyu853d
-python main.py --model model31C --msg 4gpuscsdvfb4544gwd
-python main.py --model model31C --msg 4gpudcwvrtbt445gvs
-python main.py --model model31C --msg 4gpu3g45h5nyby78mg
+python main.py --model model31C --batch_size 64 --msg 4gpubs6434rtwnhr34245t
+python main.py --model model31C --batch_size 64 --msg 4gpubs6454tbtrf3g34g3d
+python main.py --model model31C --batch_size 64 --msg 4gpubs647lk87kjyh4g3rg
+python main.py --model model31C --batch_size 64 --msg 4gpubs644gvr3ece2f27sd
+python main.py --model model31C --batch_size 64 --msg 4gpubs6412dwdvrnyu853d
+python main.py --model model31C --batch_size 64 --msg 4gpubs64scsdvfb4544gwd
+python main.py --model model31C --batch_size 64 --msg 4gpubs64dcwvrtbt445gvs
+python main.py --model model31C --batch_size 64 --msg 4gpubs643g45h5nyby78mg
