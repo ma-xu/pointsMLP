adbd52d5bc3cf21af7e58a506bbbb6956f3f5c1c
Xu Ma <xuma@my.unt.edu>
2021-08-31 22:04:18 -0400

    Update train_model31c.sh

--- classification/sbatch/train_model31c.sh
+++ classification/sbatch/train_model31c.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=voting_model31C_20210829112651
+#SBATCH --job-name=train_model31C
 #SBATCH --mail-type=END,FAIL,BEGIN
 #SBATCH --mail-user=ma.xu1@northeastern.edu
 #SBATCH -N 1
@@ -8,8 +8,8 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32Gb
 #SBATCH --time=1-00:00:00
-#SBATCH --ntasks=4
 #SBATCH --output=nohup/out_%j.log
-conda activate point
+
+source activate point
 cd /scratch/ma.xu1/pointsMLP/classification/
-python voting.py --model model31C --msg 20210829112651 --epoch 200
+python main.py --model model31C --epoch 300 --workers 4 --learning_rate 0.3
