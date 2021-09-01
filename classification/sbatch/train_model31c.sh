653de9cb63ba008b008772d8b977dfbbec3faa26
Xu Ma <xuma@my.unt.edu>
2021-08-31 22:04:58 -0400

    Update train_model31c.sh

--- classification/sbatch/train_model31c.sh
+++ classification/sbatch/train_model31c.sh
@@ -8,7 +8,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32Gb
 #SBATCH --time=1-00:00:00
-#SBATCH --output=nohup/out_%j.log
+#SBATCH --output=/scratch/ma.xu1/pointsMLP/classification/nohup/out_%j.log
 
 source activate point
 cd /scratch/ma.xu1/pointsMLP/classification/
