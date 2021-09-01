63ba0d31345eabd37ac75d8c3d78f87dc543e3eb
Xu Ma <xuma@my.unt.edu>
2021-08-31 20:25:26 -0400

    Update voting.sh

--- classification/voting.sh
+++ classification/voting.sh
@@ -9,7 +9,7 @@
 #SBATCH --mem=32Gb
 #SBATCH --time=1-00:00:00
 #SBATCH --ntasks=2
-#SBATCH --output=nohup/out_%j.log
+#SBATCH --output=out_%j.log
 conda activate point
 cd /scratch/ma.xu1/pointsMLP/classification/
 python voting.py --model model31C --msg 20210829112651 --epoch 200
