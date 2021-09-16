d7f2e8acd03c230e6e80cacb7436a2163de7b122
Xu Ma <xuma@my.unt.edu>
2021-09-16 14:49:52 -0400

    update

---
+++ classification/sbatch/pointsformer2/fugpu/train_pointsformer2B_wd2e-4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --mail-type=END
+#SBATCH --mail-user=ma.xu1@northeastern.edu
+#SBATCH -N 1
+#SBATCH -p fugpu
+#SBATCH --gres=gpu:1
+#SBATCH --output=%j.log
+
+source activate point
+cd /scratch/ma.xu1/pointsMLP/classification/
+python main.py --model pointsformer2B --epoch 300 --workers 4 --weight_decay 2e-4
