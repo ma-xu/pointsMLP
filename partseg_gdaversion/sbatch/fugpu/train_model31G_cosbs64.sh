6225bce81b81fdc52e429cbdb69ffdf0fb02b68e
Xu Ma <xuma@my.unt.edu>
2021-09-19 16:29:31 -0400

    update

--- partseg_gdaversion/sbatch/fugpu/train_model31G_cosbs64.sh
+++ partseg_gdaversion/sbatch/fugpu/train_model31G_cosbs64.sh
@@ -9,4 +9,4 @@
 
 source activate point
 cd /scratch/ma.xu1/pointsMLP/partseg_gdaversion/
-python main.py --model PointMLP31G --exp_name cosbs64 --scheduler cos --batch_size 64
+python main.py --model model31G --exp_name cosbs64 --scheduler cos --batch_size 64
