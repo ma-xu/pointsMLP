fadfebdabfd147939eddf19d6644fdf1e8b86fb4
Xu Ma <xuma@my.unt.edu>
2021-09-22 20:36:42 -0400

    Update S3DISDataLoader.py

--- segmentation/S3DISDataLoader.py
+++ segmentation/S3DISDataLoader.py
@@ -9,22 +9,22 @@ def rotate_point_cloud_z(batch_data):
     """ Randomly rotate the point clouds to augument the dataset
         rotation is per shape based along up direction
         Input:
-          BxNx3 array, original batch of point clouds
+          Nx3 array, original 1 of point clouds
         Return:
-          BxNx3 array, rotated batch of point clouds
+          Nx3 array, rotated 1 of point clouds
     """
-    rotated_data = np.zeros(batch_data.shape, dtype=np.float32)
-    for k in range(batch_data.shape[0]):
-        rotation_angle = np.random.uniform() * 2 * np.pi
-        cosval = np.cos(rotation_angle)
-        sinval = np.sin(rotation_angle)
-        rotation_matrix = np.array([[cosval, sinval, 0],
-                                    [-sinval, cosval, 0],
-                                    [0, 0, 1]])
-        shape_pc = batch_data[k, ...]
-        rotated_data[k, ...] = np.dot(shape_pc.reshape((-1, 3)), rotation_matrix)
+    rotation_angle = np.random.uniform() * 2 * np.pi
+    cosval = np.cos(rotation_angle)
+    sinval = np.sin(rotation_angle)
+    rotation_matrix = np.array([[cosval, sinval, 0],
+                                [-sinval, cosval, 0],
+                                [0, 0, 1]])
+    rotated_data = np.dot(batch_data.reshape((-1, 3)), rotation_matrix)
     return rotated_data
 
+# data = np.random.random([4,3])
+# out = rotate_point_cloud_z(data)
+# print(out.shape)
 
 class S3DISDataset(Dataset):
     def __init__(self, split='train', data_root='trainval_fullarea', num_point=4096, test_area=5, block_size=1.0, sample_rate=1.0, transform=None):
