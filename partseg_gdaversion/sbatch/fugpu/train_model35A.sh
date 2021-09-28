cbc7098b30ee02a10b2c3e5740d74110b7442b3e
Xu Ma <xuma@my.unt.edu>
2021-09-27 20:46:05 -0400

    update

--- partseg_gdaversion/sbatch/fugpu/train_model35A.sh
+++ partseg_gdaversion/sbatch/fugpu/train_model35A.sh
@@ -9,4 +9,4 @@
 
 source activate point
 cd /scratch/ma.xu1/pointsMLP/partseg_gdaversion/
-python main.py --model model35A --exp_name cos32 --scheduler cos --epochs 250
+python main.py --model model35A --exp_name cos32 --scheduler cos
