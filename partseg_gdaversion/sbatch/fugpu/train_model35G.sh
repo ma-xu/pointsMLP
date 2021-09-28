1e3cf812bf4c8e271ed66350be1809a07efa5c79
Xu Ma <xuma@my.unt.edu>
2021-09-27 20:41:02 -0400

    Update train_model35G.sh

--- partseg_gdaversion/sbatch/fugpu/train_model35G.sh
+++ partseg_gdaversion/sbatch/fugpu/train_model35G.sh
@@ -3,10 +3,11 @@
 #SBATCH --mail-user=ma.xu1@northeastern.edu
 #SBATCH -N 1
 #SBATCH -p fugpu
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=28
 #SBATCH --gres=gpu:4
 #SBATCH --output=%j.log
 
 source activate point
 cd /scratch/ma.xu1/pointsMLP/partseg_gdaversion/
-python main.py --model model35G --exp_name cos32 --scheduler cos --epochs 250
+python main.py --model model35G --exp_name cos_bs32 --scheduler cos --epochs 250 &
+python main.py --model model35A --exp_name cos_bs32 --scheduler cos --epochs 250 &
