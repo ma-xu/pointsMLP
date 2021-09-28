1c7b58b50397feb0372aebef2f044894e524c4d6
Xu Ma <xuma@my.unt.edu>
2021-09-27 20:32:01 -0400

    update

---
+++ partseg_gdaversion/sbatch/fugpu/train_model35G.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --mail-type=END
+#SBATCH --mail-user=ma.xu1@northeastern.edu
+#SBATCH -N 1
+#SBATCH -p fugpu
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:4
+#SBATCH --output=%j.log
+
+source activate point
+cd /scratch/ma.xu1/pointsMLP/partseg_gdaversion/
+python main.py --model model35G --exp_name cos32 --scheduler cos --epochs 250
