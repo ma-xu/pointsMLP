1508b634a06aba86460af717fe2446a6ae9cca6d
Xu Ma <xuma@my.unt.edu>
2021-09-30 03:12:16 -0400

    update

---
+++ classification/sbatch/ablation/ablation_pointnet.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --mail-type=END
+#SBATCH --mail-user=ma.xu1@northeastern.edu
+#SBATCH -N 1
+#SBATCH -p fugpu
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH --output=%j.log
+
+source activate point
+cd /scratch/ma.xu1/pointsMLP/classification/
+python main.py --model PointNet --epoch 300 --workers 4
