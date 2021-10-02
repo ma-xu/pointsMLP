91c9ca56969f3d9f8dbb33e6bde62885bc86e8a6
13952522076 <xuma@my.unt.edu>
2021-10-02 14:06:17 -0400

    1

--- classification/models/summary.py
+++ classification/models/summary.py
@@ -3,6 +3,7 @@ from CurveNet import CurveNet as net  #2,142,672
 # from modelelite3 import modelelite3X10 as net
 # from GBNet import GBNet as net  # 8,798,048
 # from GDANet import GDANET as net  # 939,176
-from GBNet import DGCNN as net #
+# from GBNet import DGCNN as net #1,812,648
+from pointConv import PointConv as net #
 model = net().cuda()
 print(summary(model, (3,1024)))
