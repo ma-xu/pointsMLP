f8805ed3e2f1ac4df86c52c9f68d1f3aaad69909
Xu Ma <xuma@my.unt.edu>
2021-10-02 13:14:48 -0400

    Update train_modelelite3X10_wd2e-4_fugpu.sh

--- classification/sbatch/elite3/train_modelelite3X10_wd2e-4_fugpu.sh
+++ classification/sbatch/elite3/train_modelelite3X10_wd2e-4_fugpu.sh
@@ -3,7 +3,7 @@
 #SBATCH --mail-user=ma.xu1@northeastern.edu
 #SBATCH -N 1
 #SBATCH -p fugpu
-#SBATCH --gres=gpu::1
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32Gb
 #SBATCH --time=1-00:00:00
