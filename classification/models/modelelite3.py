4f8da1249b4efe5c3161d4a29afab38351b48e44
Xu Ma <xuma@my.unt.edu>
2021-10-02 02:29:27 -0400

    Update modelelite3.py

--- classification/models/modelelite3.py
+++ classification/models/modelelite3.py
@@ -22,7 +22,7 @@ import torch.nn.functional as F
 from einops.layers.torch import Rearrange
 
 
-# from pointnet2_ops import pointnet2_utils
+from pointnet2_ops import pointnet2_utils
 
 
 def get_activation(activation):
@@ -173,8 +173,8 @@ class LocalGrouper(nn.Module):
         xyz = xyz.contiguous()  # xyz [btach, points, xyz]
 
         # fps_idx = torch.multinomial(torch.linspace(0, N - 1, steps=N).repeat(B, 1).to(xyz.device), num_samples=self.groups, replacement=False).long()
-        fps_idx = farthest_point_sample(xyz, self.groups).long()
-        # fps_idx = pointnet2_utils.furthest_point_sample(xyz, self.groups).long()  # [B, npoint]
+        # fps_idx = farthest_point_sample(xyz, self.groups).long()
+        fps_idx = pointnet2_utils.furthest_point_sample(xyz, self.groups).long()  # [B, npoint]
         new_xyz = index_points(xyz, fps_idx)  # [B, npoint, 3]
         new_points = index_points(points, fps_idx)  # [B, npoint, d]
 
@@ -567,37 +567,37 @@ def modelelite3X8(num_classes=40, **kwargs) -> modelelite3:  # 0.95M
     return modelelite3(points=1024, class_num=num_classes, embed_dim=32, groups=1, res_expansion=0.125,
                    activation="relu", bias=False, use_xyz=False, normalize="anchor",
                    dim_expansion=[2, 2, 2, 2], pre_blocks=[1, 1, 2, 1], pos_blocks=[1, 1, 1, 1],
-                   k_neighbors=[20, 20, 20, 20], reducers=[2, 2, 2, 2], **kwargs)
+                   k_neighbors=[24,24,24,24], reducers=[2, 2, 2, 2], **kwargs)
 
 def modelelite3X9(num_classes=40, **kwargs) -> modelelite3:  # 1.59M
     return modelelite3(points=1024, class_num=num_classes, embed_dim=64, groups=1, res_expansion=0.125,
                    activation="relu", bias=False, use_xyz=False, normalize="anchor",
                    dim_expansion=[2, 2, 2, 1], pre_blocks=[1, 1, 1, 1], pos_blocks=[1, 1, 1, 1],
-                   k_neighbors=[20, 20, 20, 20], reducers=[2, 2, 2, 2], **kwargs)
+                   k_neighbors=[24,24,24,24], reducers=[2, 2, 2, 2], **kwargs)
 
 def modelelite3X10(num_classes=40, **kwargs) -> modelelite3:  # 0.72M
     return modelelite3(points=1024, class_num=num_classes, embed_dim=32, groups=1, res_expansion=0.25,
                    activation="relu", bias=False, use_xyz=False, normalize="anchor",
                    dim_expansion=[2, 2, 2, 1], pre_blocks=[1, 1, 2, 1], pos_blocks=[1, 1, 2, 1],
-                   k_neighbors=[20, 20, 20, 20], reducers=[2, 2, 2, 2], **kwargs)
+                   k_neighbors=[24,24,24,24], reducers=[2, 2, 2, 2], **kwargs)
 
 def modelelite3X11(num_classes=40, **kwargs) -> modelelite3:  # 0.78M
     return modelelite3(points=1024, class_num=num_classes, embed_dim=48, groups=1, res_expansion=0.25,
                    activation="relu", bias=False, use_xyz=False, normalize="anchor",
                    dim_expansion=[1, 2, 2, 2], pre_blocks=[1, 1, 2, 1], pos_blocks=[1, 1, 2, 1],
-                   k_neighbors=[20, 20, 20, 20], reducers=[2, 2, 2, 2], **kwargs)
+                   k_neighbors=[24,24,24,24], reducers=[2, 2, 2, 2], **kwargs)
 
 def modelelite3X12(num_classes=40, **kwargs) -> modelelite3:  # 0.98M
     return modelelite3(points=1024, class_num=num_classes, embed_dim=64, groups=1, res_expansion=0.25,
                    activation="relu", bias=False, use_xyz=False, normalize="anchor",
                    dim_expansion=[1, 2, 2, 2], pre_blocks=[1, 1, 1, 1], pos_blocks=[1, 1, 1, 0],
-                   k_neighbors=[20, 20, 20, 20], reducers=[2, 2, 2, 2], **kwargs)
+                   k_neighbors=[24,24,24,24], reducers=[2, 2, 2, 2], **kwargs)
 
 def modelelite3X13(num_classes=40, **kwargs) -> modelelite3:  # 0.94M
     return modelelite3(points=1024, class_num=num_classes, embed_dim=64, groups=1, res_expansion=0.125,
                    activation="relu", bias=False, use_xyz=False, normalize="anchor",
                    dim_expansion=[1, 2, 2, 2], pre_blocks=[1, 2, 2, 2], pos_blocks=[1, 1, 0, 0],
-                   k_neighbors=[20, 20, 20, 20], reducers=[2, 2, 2, 2], **kwargs)
+                   k_neighbors=[24,24,24,24], reducers=[2, 2, 2, 2], **kwargs)
 
 if __name__ == '__main__':
     # data = torch.rand(2, 128, 10)
