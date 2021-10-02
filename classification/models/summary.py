ccef9f9246f3dfc0434a9334af59eaee117cd0f2
13952522076 <xuma@my.unt.edu>
2021-10-02 13:55:10 -0400

    update

--- classification/models/summary.py
+++ classification/models/summary.py
@@ -1,4 +1,5 @@
 from torchsummary import summary
-from CurveNet import CurveNet as net
+from CurveNet import CurveNet as net  #2,142,672
+from modelelite3 import modelelite3X10 as net
 model = net().cuda()
 print(summary(model, (3,1024)))
