c0f04e5f950d1c1604a5951b95b3f4447d15ebbb
13952522076 <xuma@my.unt.edu>
2021-10-02 14:03:28 -0400

    1

--- classification/models/summary.py
+++ classification/models/summary.py
@@ -1,6 +1,7 @@
 from torchsummary import summary
 from CurveNet import CurveNet as net  #2,142,672
 # from modelelite3 import modelelite3X10 as net
-from GBNet import GBNet as net
+# from GBNet import GBNet as net  # 8,798,048
+from GDANet import GDANET as net  #
 model = net().cuda()
 print(summary(model, (3,1024)))
