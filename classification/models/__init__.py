f1c705b53b992348c017ce7c825d4b6c447c69e3
Xu Ma <xuma@my.unt.edu>
2021-10-02 16:47:09 -0400

    update

--- classification/models/__init__.py
+++ classification/models/__init__.py
@@ -18,6 +18,8 @@ from .model25 import model25H
 from .model26 import model26H, model26A, model26B, model26C, model26D
 from .model31 import model31A, model31B, model31C, model31D, model31E, model31F, model31G, model31H, model31I, \
     model31J, model31K, model31L, model31M, model31N
+from .model31NoRes import model31CNoRes
+
 
 from .model32 import model32A,model32C,model32D, model32E, model32F, model32G, model32H, model32I, model32J, model32K,\
     model32L, model32M, model32N, model32A2, model32A3, model32A4
