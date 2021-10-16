7352603b0ca250b230e2a49b870a80dc29adf2f9
Xu Ma <xuma@my.unt.edu>
2021-10-15 23:29:55 -0400

    update

--- partseg_v2/sbatch/fugpu/train_model40D_cos_64.sh
+++ partseg_v2/sbatch/fugpu/train_model40D_cos_64.sh
@@ -3,10 +3,10 @@
 #SBATCH --mail-user=ma.xu1@northeastern.edu
 #SBATCH -N 1
 #SBATCH -p fugpu
-#SBATCH --cpus-per-task=24
+#SBATCH --cpus-per-task=14
 #SBATCH --gres=gpu:4
 #SBATCH --output=%j.log
 
 source activate point
 cd /scratch/ma.xu1/pointsMLP/partseg_v2/
-python main.py --model model40D --scheduler cos --batch_size 64 --workers 24
+python main.py --model model40D --scheduler cos --batch_size 64 --workers 14
