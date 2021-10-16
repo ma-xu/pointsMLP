1dd20294ad98e7a8eea7005c75c929b5eaea9e1c
Xu Ma <xuma@my.unt.edu>
2021-10-15 23:24:24 -0400

    update

--- partseg_v2/model/__init__.py
+++ partseg_v2/model/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import absolute_import
 
-from .pointMLP40 import model40G  # instacne-wise
+from .pointMLP40 import model40A, model40B, model40C, model40D, model40E  # instacne-wise
--- partseg_v2/sbatch/fugpu/train_model40B_cos_64.sh
+++ partseg_v2/sbatch/fugpu/train_model40B_cos_64.sh
@@ -3,10 +3,10 @@
 #SBATCH --mail-user=ma.xu1@northeastern.edu
 #SBATCH -N 1
 #SBATCH -p fugpu
-#SBATCH --cpus-per-task=12
-#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-task=24
+#SBATCH --gres=gpu:4
 #SBATCH --output=%j.log
 
 source activate point
 cd /scratch/ma.xu1/pointsMLP/partseg_v2/
-python main.py --model model40D --scheduler cos --batch_size 32
+python main.py --model model40B --scheduler cos --batch_size 64 --workers 24
