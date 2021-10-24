b2fb9688da0740132f04101b3c6c9a88362e4f55
Xu Ma <xuma@my.unt.edu>
2021-10-23 22:29:28 -0400

    update

---
+++ partseg_curvenet/sbtach/fugpu/train_pformer34A_cos.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --mail-type=END
+#SBATCH --mail-user=ma.xu1@northeastern.edu
+#SBATCH -N 1
+#SBATCH -p fugpu
+#SBATCH --cpus-per-task=6
+#SBATCH --gres=gpu:2
+#SBATCH --output=%j.log
+
+source activate point
+cd /scratch/ma.xu1/pointsMLP/partseg_curvenet/
+python main.py --model pformer34A --exp_name=Nov_cos --workers 6 --use_sgd False
