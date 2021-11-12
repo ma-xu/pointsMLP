904dabbec91c5d884f3e7a82f6fd056e6e664c8f
Xu Ma <xuma@my.unt.edu>
2021-11-12 05:38:44 -0500

    update

--- classification/sbatch/rebuttal/train_DGCNNAffine.sh
+++ classification/sbatch/rebuttal/train_DGCNNAffine.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --mail-type=END,FAIL,BEGIN
+#SBATCH --mail-type=END
 #SBATCH --mail-user=ma.xu1@northeastern.edu
 #SBATCH -N 1
 #SBATCH -p multigpu
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32Gb
 #SBATCH --time=1-00:00:00
-#SBATCH --output=../nohup/%j.log
+#SBATCH --output=%j.log
 
 source activate point
 cd /scratch/ma.xu1/pointsMLP/classification/
