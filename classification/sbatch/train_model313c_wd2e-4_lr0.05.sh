50a50afb0ccf267cdc66d5422e2bc10368a935db
Xu Ma <xuma@my.unt.edu>
2022-02-06 14:29:52 -0500

    update

---
+++ classification/sbatch/train_model313c_wd2e-4_lr0.05.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --mail-type=END,FAIL,BEGIN
+#SBATCH --mail-user=ma.xu1@northeastern.edu
+#SBATCH -N 1
+#SBATCH -p multigpu
+#SBATCH --gres=gpu:v100-sxm2:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32Gb
+#SBATCH --time=1-00:00:00
+#SBATCH --output=../nohup/%j.log
+
+source activate point
+cd /scratch/ma.xu1/pointsMLP/classification/
+python main.py --model model313C --epoch 300 --workers 4 --weight_decay 2e-4 --learning_rate 0.05
