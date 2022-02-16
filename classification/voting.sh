e8e49b4c2f68f28dbe7f9061f949c8c561e6cec2
Xu Ma <xuma@my.unt.edu>
2022-02-16 10:40:12 -0500

    update

--- classification/voting.sh
+++ classification/voting.sh
@@ -12,4 +12,4 @@
 source /home/ma.xu1/.condarc
 source activate point
 cd /scratch/ma.xu1/pointsMLP/classification/
-python voting.py --model model313C --msg 20220209053148 --NUM_PEPEAT 200
+python voting.py --model model313C --msg 20220209053148-404 --NUM_PEPEAT 200
--- classification/voting_fugpu.sh
+++ classification/voting_fugpu.sh
@@ -11,4 +11,4 @@
 source /home/ma.xu1/.condarc
 source activate point
 cd /scratch/ma.xu1/pointsMLP/classification/
-python voting.py --model model313C --msg 20220209053148 --NUM_PEPEAT 200
+python voting.py --model model313C --msg 20220209053148-404 --NUM_PEPEAT 200
